ec88369399e20a8c020fb42e5bd3aeb6b713af44
Nazar <nazarkutsenko777@gmail.com>
2026-04-19 14:47:55 +0300

    Виправлено шляхи до файлів у main.py та додано дані в input.txt

--- src/main.py
+++ src/main.py
@@ -4,20 +4,17 @@ from analysis import calc_average, calc_median, calc_minimum, calc_maximum
 def main():
     print("=== Консольний калькулятор статистичних показників ===")
     
-    # 1. Зчитування даних (Студент А)
-    data = read_data("../data/input.txt")
+    data = read_data("data/input.txt")
     
     if not data:
         print("Програму завершено через відсутність або помилку даних.")
         return
 
-    # 2. Обчислення (Студент Б)
     avg_val = calc_average(data)
     med_val = calc_median(data)
     min_val = calc_minimum(data)
     max_val = calc_maximum(data)
 
-    # 3. Формування результату та виведення в консоль (Студент А)
     result = (
         f"Статистика за наданими даними:\n"
         f"-------------------------------\n"
@@ -30,8 +27,7 @@ def main():
     
     print("\n" + result)
     
-    # 4. Збереження у файл
-    save_data("../data/output.txt", result)
+    save_data("data/output.txt", result)
 
 if __name__ == "__main__":
     main()
